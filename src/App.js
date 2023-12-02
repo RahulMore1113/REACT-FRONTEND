a558e3880b6fad8b79349cf7e7bfbbbfe0a19b53
Rahul <rahulmore1113@gmail.com>
2023-12-02 18:53:46 +0530

    REACT SPRING INTEGRATION

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import HeaderComponent from './Component/HeaderComponent';
+import {BrowserRouter as Router} from 'react-router-dom';
+import { Routes, Route } from 'react-router';
+import ListEmployeeComponent from './Component/ListEmployeeComponent';
+import FooterComponent from './Component/FooterComponent';
+import CreateEmployeeComponent from './Component/CreateEmployeeComponent';
+import UpdateEmployeeComponent from './Component/UpdateEmployeeComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <HeaderComponent />
+        <div className='container'>
+          <Routes>
+            <Route path='/' element = {<ListEmployeeComponent />} />
+            <Route path='/employees' element = {<ListEmployeeComponent />} />
+            <Route path='/add-employee' element = {<CreateEmployeeComponent />} />
+            <Route path='/update-employee/:id' element = {<UpdateEmployeeComponent />} />
+          </Routes>
+        </div>
+        <FooterComponent />
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
